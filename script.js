3c1ee11dadc0303489005d8ffa5cf7d2dc843b2e
shahrokh <shahrokhelmi@gmail.com>
2021-12-30 11:55:53 +0330

    third

--- script.js
+++ script.js
@@ -50,38 +50,73 @@
 // }
 //
 // calcAgeArrow(1993)
-
-const jonas = {
-    year:1991,
-    calcAge:function(){
-        // console.log(this)
-        // console.log(this.year)
-        const self = this;
-        const isMillennial =  function (){
-            console.log(self)
-        };
-        isMillennial();
-
-    },
+//
+// const jonas = {
+//     year:1991,
+//     calcAge:function(){
+//         // console.log(this)
+//         // console.log(this.year)
+//         const self = this;
+//         const isMillennial =  function (){
+//             console.log(self)
+//         };
+//         isMillennial();
+//
+//     },
     // lastName:'elmi',
     // greet:()=>console.log(`hi ${this.lastName}`),
-}
+// }
 
-jonas.calcAge();
+// jonas.calcAge();
 
 //
 // jonas.calcAge();
 //
 //
-const matilda = {
-    year:2017,
-};
-
+// const matilda = {
+//     year:2017,
+// };
 //
-matilda.calcAge = jonas.calcAge;
-matilda.calcAge();
+// //
+// matilda.calcAge = jonas.calcAge;
+// matilda.calcAge();
 
 // var lastName = 'elll';
 // const aaaa = 'hi';
 // console.log(this)
 // jonas.greet();
+
+// let age = 30;
+// let oldAge = age;
+//
+// age = 31;
+//
+// console.log(oldAge);
+//
+// const me = {
+//     age:29,
+//     name:'shahrokh'
+// };
+//
+// const friend = me;
+//
+// me.age = 30;
+//
+// console.log(me)
+// console.log(friend)
+//
+
+let lastName = 'elmi';
+let oldLastName = lastName;
+
+lastName = 'samani';
+console.log(lastName, oldLastName);
+
+const jessica = {
+    firstName: 'Jessica',
+    lastName:'Williams',
+    age:27
+}
+const marriedJessica = Object.assign({}, jessica);
+
+marriedJessica.lastName = 'Davis';
